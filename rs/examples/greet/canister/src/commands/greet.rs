5aa186cfe063ce64dde745c36b85351ec6e00682
megrogan <megrogan@gmail.com>
2025-01-13 17:17:57 +0000

    Only log errors

--- rs/examples/greet/canister/src/commands/greet.rs
+++ rs/examples/greet/canister/src/commands/greet.rs
@@ -8,12 +8,15 @@ pub fn greet(context: BotCommandContext) -> Result<SuccessResult, InternalError>
     let text = format!("hello @UserId({user_id})");
 
     // Send the message to OpenChat but don't wait for the response
-    let message = OPENCHAT_CLIENT.send_text_message(&context, text, true, |args, response| {
-        ic_cdk::println!("send_text_message: {args:?}, {response:?}");
-        if response.is_ok_and(|r| r.0.is_ok()) {
-            state::mutate(|state| state.increment_greets_sent());
-        }
-    });
+    let message =
+        OPENCHAT_CLIENT.send_text_message(&context, text, true, |args, response| match response {
+            Ok(result) if result.0.is_ok() => {
+                state::mutate(|state| state.increment_jokes_sent());
+            }
+            error => {
+                ic_cdk::println!("greet send_text_message: {args:?}, {error:?}");
+            }
+        });
 
     Ok(SuccessResult {
         message: Some(message),
--- rs/examples/greet/canister/src/commands/joke.rs
+++ rs/examples/greet/canister/src/commands/joke.rs
@@ -7,12 +7,15 @@ pub fn joke(context: BotCommandContext) -> Result<SuccessResult, InternalError> 
     let text = state::read(|state| state.get_random_joke());
 
     // Send the message to OpenChat but don't wait for the response
-    let message = OPENCHAT_CLIENT.send_text_message(&context, text, true, |args, response| {
-        ic_cdk::println!("send_text_message: {args:?}, {response:?}");
-        if response.is_ok_and(|r| r.0.is_ok()) {
-            state::mutate(|state| state.increment_jokes_sent());
-        }
-    });
+    let message =
+        OPENCHAT_CLIENT.send_text_message(&context, text, true, |args, response| match response {
+            Ok(result) if result.0.is_ok() => {
+                state::mutate(|state| state.increment_jokes_sent());
+            }
+            error => {
+                ic_cdk::println!("joke send_text_message: {args:?}, {error:?}");
+            }
+        });
 
     Ok(SuccessResult {
         message: Some(message),
